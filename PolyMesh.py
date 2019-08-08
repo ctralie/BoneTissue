c6efd6ebd1fc38cd859ffead73884d7eaf838d91
Chris Tralie <chris.tralie@gmail.com>
2019-08-08 08:21:47 -0400

    Removing Geom3D

---
+++ PolyMesh.py
@@ -0,0 +1,82 @@
+"""
+Programmer: Chris Tralie
+Purpose: Some simple routines for dealing with triangle meshes
+"""
+
+#Return VPos, VColors, and ITris without creating any structure
+#(Assumes triangle mesh)
+def loadOffFileExternal(filename):
+    """
+    Load a .off (or .coff) triangle mesh file
+    Parameters
+    ----------
+    filename: string
+        Path to the .off file
+    Returns
+    -------
+    VPos: ndarray(N, 3, dtype=np.float64)
+        Positions of the vertices
+    VColors: ndarray(N, 3, dtype=np.float64)
+        Colors of the vertices (if .coff file)
+    ITris: ndarray(M, 3, dtype=np.int32)
+        The indices of the triangles
+    """
+    fin = open(filename, 'r')
+    nVertices = 0
+    nFaces = 0
+    lineCount = 0
+    face = 0
+    vertex = 0
+    divideColor = False
+    VPos = np.zeros((0, 3))
+    VColors = np.zeros((0, 3))
+    ITris = np.zeros((0, 3))
+    for line in fin:
+        lineCount = lineCount+1
+        fields = line.split() #Splits whitespace by default
+        if len(fields) == 0: #Blank line
+            continue
+        if fields[0][0] in ['#', '\0', ' '] or len(fields[0]) == 0:
+            continue
+        #Check section
+        if nVertices == 0:
+            if fields[0] == "OFF" or fields[0] == "COFF":
+                if len(fields) > 2:
+                    fields[1:4] = [int(field) for field in fields]
+                    [nVertices, nFaces, nEdges] = fields[1:4]
+                    print "nVertices = %i, nFaces = %i"%(nVertices, nFaces)
+                    #Pre-allocate vertex arrays
+                    VPos = np.zeros((nVertices, 3))
+                    VColors = np.zeros((nVertices, 3))
+                    ITris = np.zeros((nFaces, 3))
+                if fields[0] == "COFF":
+                    divideColor = True
+            else:
+                fields[0:3] = [int(field) for field in fields]
+                [nVertices, nFaces, nEdges] = fields[0:3]
+                VPos = np.zeros((nVertices, 3))
+                VColors = np.zeros((nVertices, 3))
+                ITris = np.zeros((nFaces, 3))
+        elif vertex < nVertices:
+            fields = [float(i) for i in fields]
+            P = [fields[0],fields[1], fields[2]]
+            color = np.array([0.5, 0.5, 0.5]) #Gray by default
+            if len(fields) >= 6:
+                #There is color information
+                if divideColor:
+                    color = [float(c)/255.0 for c in fields[3:6]]
+                else:
+                    color = [float(c) for c in fields[3:6]]
+            VPos[vertex, :] = P
+            VColors[vertex, :] = color
+            vertex = vertex+1
+        elif face < nFaces:
+            #Assume the vertices are specified in CCW order
+            fields = [int(i) for i in fields]
+            ITris[face, :] = fields[1:fields[0]+1]
+            face = face+1
+    fin.close()
+    VPos = np.array(VPos, np.float64)
+    VColors = np.array(VColors, np.float64)
+    ITris = np.array(ITris, np.int32)
+    return (VPos, VColors, ITris)
