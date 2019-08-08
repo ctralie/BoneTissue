a2a57b2ab90f2f7fd0f8fd7c2a771a2247b58a7c
Chris Tralie <chris.tralie@gmail.com>
2019-08-08 16:33:27 -0400

    Adding code to sample points on the convex hull of the samples

--- BoneData.py
+++ BoneData.py
@@ -16,7 +16,7 @@ import json
 import subprocess
 import cechmate as cm
 from persim import plot_diagrams as plot_dgms
-from PolyMesh import loadOffFileExternal
+from PolyMesh import *
 
 def getPlotName(f, ext):
     s = f.split("qrtosso_")[1]
@@ -26,41 +26,52 @@ def getPlotName(f, ext):
     s = "%s_%s"%(s1, s2)
     return s
 
-def getBoneAlpha(filename, sample_hull = False):
+def getBoneAlpha(filename, hull_samples = 0):
     """
     Compute the Alpha filtration of a bone specimen
     Parameters
     ----------
     filename: string
         Path to 3D mesh file containing the bone
-    sample_hull: boolean
-        Whether to sample points on the convex hull to help
-        increase the birth time of H2
+    hull_samples: int
+        The number of points to sample on the convex hull.
+        This can help increase the birth time of H2
     Returns
     -------
-    Y: ndarray(N, 3)
-        The original point cloud
-    dgms: [ndarray(N0, 2), ndarray(N1, 2), ndarray(N2, 2)]
+    {'X': ndarray(N, 3)
+        The original point cloud,
+     'Y': ndarray(N+hull_samples, 3)
+        The point cloud with hull points added,
+     'dgms': [ndarray(N0, 2), ndarray(N1, 2), ndarray(N2, 2)]
         A list of diagrams for H0, H1, and H2
+    }
     """
     ## Step 1: Load mesh by using meshlab to convert it to off
     ## In the process, remove duplicate vertices
     filetemp = "%s.off"%filename[0:-4]
     subprocess.call(["meshlabserver", "-i", filename, "-s", "removedup.mlx", "-o", filetemp])
-    (Y, _, _) = loadOffFileExternal(filetemp)
-    Y = Y + 0.001*np.random.randn(Y.shape[0], Y.shape[1])
+    (X, _, _) = loadOffFile(filetemp)
+    # Add some noise to put into general position (since points were sampled
+    # on a grid, bad things can happen otherwise)
+    X = X + 0.001*np.random.randn(X.shape[0], X.shape[1])
     
     ## Step 2: Sample convex hull, if requested
-    ##TODO: FINISH THIS
+    Y = np.array(X)
+    if hull_samples > 0:
+        hull = ConvexHull(X)
+        Ps, _ = randomlySamplePoints(X, hull.simplices, hull_samples)
+        saveOffFile("hull%i.off"%hull_samples, Ps, np.array([]), np.array([]))
+        Y = np.concatenate((Y, Ps))
+        saveOffFile("hull%i_withmesh.off"%hull_samples, Y, np.array([]), np.array([]))
 
     ## Step 3: Do the Alpha filtration
     alpha = cm.Alpha()
     filtration = alpha.build(Y)
     dgms = alpha.diagrams(filtration)
 
-    return Y, dgms
+    return {'X':X, 'Y':Y, 'dgms':dgms}
 
-def computeAllPersistenceDiagrams():
+def computeAllPersistenceDiagrams(hull_samples=0):
     """
     Compute all alpha filtrations for the bone dataset, and save
     the results along with the parameters in the dataset
@@ -74,7 +85,8 @@ def computeAllPersistenceDiagrams():
     plt.figure(figsize=(16, 8))
     for i, f in enumerate(glob.glob("BoneData/FIGURE_PORTION/*/*/*.stl")):
         print(f)
-        Y, dgms = getBoneAlpha(f)
+        res = getBoneAlpha(f, hull_samples=hull_samples)
+        dgms, Y = res['dgms'], res['Y']
         specimen = f.split("qrtosso_")[1][0:2]
         res = copy.deepcopy(specimen2val[specimen])
         plt.clf()
@@ -85,12 +97,12 @@ def computeAllPersistenceDiagrams():
         plt.subplot(122)
         plot_dgms(dgms)
         plt.title("Alpha Persistence Diagrams")
-        plt.savefig("PDs/%i.png"%i, bbox_inches='tight')
+        plt.savefig("PDs/%i_%i.png"%(i, hull_samples), bbox_inches='tight')
         for k, dgm in enumerate(dgms):
             res['H%i'%k] = dgm
         res['specimen'] = specimen
-        sio.savemat("PDs/%i.mat"%i, res)
+        sio.savemat("PDs/%i_%i.mat"%(i, hull_samples), res)
 
         
 if __name__ == '__main__':
-    computeAllPersistenceDiagrams()
+    computeAllPersistenceDiagrams(hull_samples=10000)
--- PolyMesh.py
+++ PolyMesh.py
@@ -4,9 +4,7 @@ Purpose: Some simple routines for dealing with triangle meshes
 """
 import numpy as np
 
-#Return VPos, VColors, and ITris without creating any structure
-#(Assumes triangle mesh)
-def loadOffFileExternal(filename):
+def loadOffFile(filename):
     """
     Load a .off (or .coff) triangle mesh file
     Parameters
@@ -81,3 +79,138 @@ def loadOffFileExternal(filename):
     VColors = np.array(VColors, np.float64)
     ITris = np.array(ITris, np.int32)
     return (VPos, VColors, ITris)
+
+def saveOffFile(filename, VPos, VColors, ITris):
+    """
+    Save a .off file
+    Parameters
+    ----------
+    filename: string
+        Path to which to save off file
+    VPos: ndarray(N, 3)
+        Positions of vertices
+    VColors: ndarray(N, 3)
+        RGB values of colors, or empty if all gray
+    ITris: ndarray(M, 3)
+        Indices into vertices of each triangle
+    """
+    nV = VPos.shape[0]
+    nF = ITris.shape[0]
+    fout = open(filename, "w")
+    if VColors.size == 0:
+        fout.write("OFF\n%i %i %i\n"%(nV, nF, 0))
+    else:
+        fout.write("COFF\n%i %i %i\n"%(nV, nF, 0))
+    for i in range(nV):
+        fout.write("%g %g %g"%tuple(VPos[i, :]))
+        if VColors.size > 0:
+            fout.write(" %g %g %g"%tuple(VColors[i, :]))
+        fout.write("\n")
+    for i in range(nF):
+        fout.write("3 %i %i %i\n"%tuple(ITris[i, :]))
+    fout.close()
+
+def randomlySamplePoints(VPos, ITris, NPoints, colPoints = False):
+    """
+    Randomly sample points by area on a triangle mesh.  This function is
+    extremely fast by using broadcasting/numpy operations in lieu of loops
+
+    Parameters
+    ----------
+    VPos : ndarray (N, 3)
+        Array of points in 3D
+    ITris : ndarray (M, 3)
+        Array of triangles connecting points, pointing to vertex indices
+    NPoints : int
+        Number of points to sample
+    colPoints : boolean (default False)
+        Whether the points are along the columns or the rows
+    
+    Returns
+    -------
+    (Ps : NDArray (NPoints, 3) array of sampled points, 
+     Ns : Ndarray (NPoints, 3) of normals at those points   )
+    """
+    ###Step 1: Compute cross product of all face triangles and use to compute
+    #areas and normals (very similar to code used to compute vertex normals)
+
+    #Vectors spanning two triangle edges
+    P0 = VPos[ITris[:, 0], :]
+    P1 = VPos[ITris[:, 1], :]
+    P2 = VPos[ITris[:, 2], :]
+    V1 = P1 - P0
+    V2 = P2 - P0
+    FNormals = np.cross(V1, V2)
+    FAreas = np.sqrt(np.sum(FNormals**2, 1)).flatten()
+
+    #Get rid of zero area faces and update points
+    ITris = ITris[FAreas > 0, :]
+    FNormals = FNormals[FAreas > 0, :]
+    FAreas = FAreas[FAreas > 0]
+    P0 = VPos[ITris[:, 0], :]
+    P1 = VPos[ITris[:, 1], :]
+    P2 = VPos[ITris[:, 2], :]
+
+    #Compute normals
+    NTris = ITris.shape[0]
+    FNormals = FNormals/FAreas[:, None]
+    FAreas = 0.5*FAreas
+    FNormals = FNormals
+    VNormals = np.zeros_like(VPos)
+    VAreas = np.zeros(VPos.shape[0])
+    for k in range(3):
+        VNormals[ITris[:, k], :] += FAreas[:, None]*FNormals
+        VAreas[ITris[:, k]] += FAreas
+    #Normalize normals
+    VAreas[VAreas == 0] = 1
+    VNormals = VNormals / VAreas[:, None]
+
+    ###Step 2: Randomly sample points based on areas
+    FAreas = FAreas/np.sum(FAreas)
+    AreasC = np.cumsum(FAreas)
+    samples = np.sort(np.random.rand(NPoints))
+    #Figure out how many samples there are for each face
+    FSamples = np.zeros(NTris, dtype=int)
+    fidx = 0
+    for s in samples:
+        while s > AreasC[fidx]:
+            fidx += 1
+        FSamples[fidx] += 1
+    #Now initialize an array that stores the triangle sample indices
+    tidx = np.zeros(NPoints, dtype=np.int64)
+    idx = 0
+    for i in range(len(FSamples)):
+        tidx[idx:idx+FSamples[i]] = i
+        idx += FSamples[i]
+    N = np.zeros((NPoints, 3)) #Allocate space for normals
+    idx = 0
+
+    #Vector used to determine if points need to be flipped across parallelogram
+    V3 = P2 - P1
+    V3 = V3/np.sqrt(np.sum(V3**2, 1))[:, None] #Normalize
+
+    #Randomly sample points on each face
+    #Generate random points uniformly in parallelogram
+    u = np.random.rand(NPoints, 1)
+    v = np.random.rand(NPoints, 1)
+    Ps = u*V1[tidx, :] + P0[tidx, :]
+    Ps += v*V2[tidx, :]
+    #Flip over points which are on the other side of the triangle
+    dP = Ps - P1[tidx, :]
+    proj = np.sum(dP*V3[tidx, :], 1)
+    dPPar = V3[tidx, :]*proj[:, None] #Parallel project onto edge
+    dPPerp = dP - dPPar
+    Qs = Ps - dPPerp
+    dP0QSqr = np.sum((Qs - P0[tidx, :])**2, 1)
+    dP0PSqr = np.sum((Ps - P0[tidx, :])**2, 1)
+    idxreg = np.arange(NPoints, dtype=np.int64)
+    idxflip = idxreg[dP0QSqr < dP0PSqr]
+    u[idxflip, :] = 1 - u[idxflip, :]
+    v[idxflip, :] = 1 - v[idxflip, :]
+    Ps[idxflip, :] = P0[tidx[idxflip], :] + u[idxflip, :]*V1[tidx[idxflip], :] + v[idxflip, :]*V2[tidx[idxflip], :]
+
+    #Step 3: Compute normals of sampled points by barycentric interpolation
+    Ns = u*VNormals[ITris[tidx, 1], :]
+    Ns += v*VNormals[ITris[tidx, 2], :]
+    Ns += (1-u-v)*VNormals[ITris[tidx, 0], :]
+    return (Ps, Ns)
